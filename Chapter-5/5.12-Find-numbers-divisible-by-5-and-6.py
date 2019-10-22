8ee96b0144e193108a4bba8c7f16b18f739241be
SHAHJAHAN MD. SWAJAN <53698448+smswajan@users.noreply.github.com>
2019-10-22 20:40:40 +0600

    Update 5.12-Find-numbers-divisible-by-5-and-6.py

--- Chapter-5/5.12-Find-numbers-divisible-by-5-and-6.py
+++ Chapter-5/5.12-Find-numbers-divisible-by-5-and-6.py
@@ -1,10 +1,8 @@
-count = 1
-for i in range (100, 1001):
-  if i % 5 == 0 and i % 6 == 0:
-    #ensuring that each line has a limited number of 10 numbers
-    if count%10 == 0:
-      print(format(i, "4d"))
-    else:
-      print(format(i, "4d"), end="")
-    
-    count += 1
+count = 0
+for i in range(100, 1001):
+    if i%5==0 and i%6==0:
+        count = count + 1
+        if count % 10 == 0:
+            print(format(i,"4d"))
+        else:
+            print(format(i, "4d"), end="")
