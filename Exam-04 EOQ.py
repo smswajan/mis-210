e2686124c9e9b0de73f922529324a1b9712ab089
SHAHJAHAN MD. SWAJAN <53698448+smswajan@users.noreply.github.com>
2019-10-31 22:44:14 +0600

    Update Exam-04 EOQ.py

--- Exam-04 EOQ.py
+++ Exam-04 EOQ.py
@@ -2,5 +2,5 @@ demand = eval(input("Demand Rate (i.e., 16,000 units): "))
 setup_cost = eval(input("The setup cost is (i.e., BDT 50: "))
 holding_cost = eval(input("The holding cost per item (i.e., BDT 2.5): "))
 
-eoq = ((2 * demand * setup_cost)/holding_cost)*0.5
+eoq = ((2 * demand * setup_cost)/holding_cost)**0.5
 print("According to the provided data the EOQ is:", eoq)
